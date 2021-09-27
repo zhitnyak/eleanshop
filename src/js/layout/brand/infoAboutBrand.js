93ecfc1bb3063a9a051ec7327bf3172289d7dc47
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-09-28 01:08:05 +0300

    for_brand

---
+++ src/js/layout/brand/infoAboutBrand.js
@@ -0,0 +1,7 @@
+import '../../../images/img/for_brand/img_for_brand_desctop.jpg';
+import '../../../images/img/for_brand/img_for_brand_left.jpg';
+import '../../../images/img/for_brand/img_for_brand_right.jpg';
+import '../../../images/img/for_brand/img_for_brand_svg.svg';
+import '../../../images/img/for_brand/img_for_brand_arrow.jpg';
+// import '../../../images/img/for_brand/img_for_brand_left.svg';
+// import '../../../images/img/for_brand/img_for_brand_right.svg';
